90989e44b3e374d5b65e8ed4f26e8d22bb23d5bc
Shiv <sky.shivy@gmail.com>
2021-06-23 09:35:53 +0530

    Initial Commit

---
+++ ADCBGamification/ADCBGamification.h
@@ -0,0 +1,18 @@
+//
+//  ADCBGamification.h
+//  ADCBGamification
+//
+//  Created by SKY on 23/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADCBGamification.
+FOUNDATION_EXPORT double ADCBGamificationVersionNumber;
+
+//! Project version string for ADCBGamification.
+FOUNDATION_EXPORT const unsigned char ADCBGamificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADCBGamification/PublicHeader.h>
+
+
