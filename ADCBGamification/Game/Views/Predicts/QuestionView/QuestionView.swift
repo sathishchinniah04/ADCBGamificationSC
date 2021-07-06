572f4cd3fe3c06e935d5380c7e151fb9313a65fe
Shiv <sky.shivy@gmail.com>
2021-07-06 10:12:53 +0530

    yellow color

--- ADCBGamification/Game/Views/Predicts/QuestionView/QuestionView.swift
+++ ADCBGamification/Game/Views/Predicts/QuestionView/QuestionView.swift
@@ -11,6 +11,12 @@ class QuestionView: UIView {
     var view: UIView?
     @IBOutlet weak var questionContainerView: UIView!
     
+    @IBOutlet weak var firstAnswerButton: NeumorphicButton!
+    @IBOutlet weak var secondAnswerButton: NeumorphicButton!
+    @IBOutlet weak var thirdAnswerButton: NeumorphicButton!
+    @IBOutlet weak var fourthAnswerButton: NeumorphicButton!
+    
+    
     func loaxXib() -> UIView {
         return UINib(nibName: "QuestionView", bundle: Bundle(for: Self.self)).instantiate(withOwner: self, options: nil).first as! UIView
     }
@@ -39,11 +45,17 @@ class QuestionView: UIView {
     
     func populateView() {
         setupCornerRadius()
+        buttonSetup()
     }
     
     func setupCornerRadius() {
         DispatchQueue.main.async {
             self.questionContainerView.layer.cornerRadius = self.questionContainerView.frame.height/2
         }
+    }
+    
+    func buttonSetup() {
+        firstAnswerButton.button.setTitleColor(UIColor.black, for: .normal)
+        firstAnswerButton.button.setTitle("firstAnswerButton", for: .normal)
     }
 }
