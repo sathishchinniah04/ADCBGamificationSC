36fd6359b79c4e38526c1d2295fbca9465e4d27b
Shiv <sky.shivy@gmail.com>
2021-07-08 15:40:30 +0530

    uncommitted file

---
+++ ADCBGamification/Game/Model/SpinOffers.swift
@@ -0,0 +1,19 @@
+//
+//  SpinOffers.swift
+//  ADCBGamification
+//
+//  Created by SKY on 08/07/21.
+//
+
+import Foundation
+class SpinOffers: Decodable {
+    var timestamp: String?
+    var respCode: String?
+    var respDesc: String?
+    var offers: [Offers]?
+}
+
+class Offers: Decodable {
+    var rewardId: String?
+    var rewardTitle: String?
+}
